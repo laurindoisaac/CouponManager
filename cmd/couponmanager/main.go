1aee288234fcc2c09da6bd24092d8f579b784666
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 00:52:23 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/couponmanager/main.go
@@ -0,0 +1,23 @@
+// cmd/couponmanager/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "couponmanager/internal/couponmanager"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := couponmanager.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
